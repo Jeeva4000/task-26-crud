4fc6b4f36ea44ec7ee3ef63c45398ad29172139a
Jeeva4000 <vjeeva@gmail.com>
2023-08-07 16:33:48 +0530

    new changes

---
+++ src/Data/data.js
@@ -0,0 +1,57 @@
+var data =  [
+    {
+    name : "Jeeva",
+    gender : "male",
+    batch: "B45WD",
+    qualification : "DME"
+  },
+  {
+    name: "Rajesh",
+    gender: "male",
+    batch: "B45WD",
+    qualification: "B.E"
+  },
+  {
+    name: "Aswini",
+    gender: "female",
+    batch: "B45WD",
+    qualification: "BSC IT"
+  },
+  {
+    name: "Arun",
+    gender: "male",
+    batch: "B45WD",
+    qualification: "Msc"
+  },
+  {
+    name: "Akila",
+    gender: "female",
+    batch: "B45WD",
+    qualification: "BCA"
+  },
+  {
+    name: "Siva",
+    gender: "male",
+    batch: "B45WD",
+    qualification: "B.E-Mech"
+  },
+  {
+    name: "Mahesh",
+    gender: "male",
+    batch: "-",
+    qualification: "B.E-Mech"
+  },
+  {
+    name: "Kumar",
+    gender: "male",
+    batch: "B45WD",
+    qualification: "B.E-Mech"
+  },
+  {
+    name: "Suresh",
+    gender: "male",
+    batch: "B45WD",
+    qualification: "B.E-Mech"
+  }
+  ];
+  export default data;
